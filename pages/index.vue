4a4d3c0db15e63297412aa06b6e5cc8e5c63f57f
Cocovg <Cocovanglabbeek@Hotmail.com>
2024-12-12 14:32:27 +0100

    edited content

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,21 @@
 <template>
-  <div class="overflow-hidden ">
-    <header class="w-screen">
-      <div class="">
-        <img src="public/img/header.png">
-      </div>
-     </header>
-
-    <main>
-      <div class="w-screen">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed accumsan arcu, pulvinar tristique purus.
-          Ut euismod commodo pretium. Quisque nulla orci, interdum et sem vel, accumsan mattis eros. Fusce auctor commodo feugiat.
-          Quisque justo nisi, porta sed luctus id, aliquet at nunc. Duis pretium laoreet eros sed finibus. Aliquam in augue risus.
-          Morbi tortor nibh, scelerisque vitae sagittis et, hendrerit ac velit. Nulla cursus turpis ligula, eget ultricies neque eleifend a.
-          Nulla pharetra fermentum lacus eget dignissim. Suspendisse facilisis lacus nec nibh iaculis, sit amet elementum lorem dictum.
-          Proin nec suscipit turpis. In rutrum sit amet ante vel malesuada. Curabitur orci metus, commodo dictum scelerisque eu, aliquet vel ex.
-          Phasellus et sapien condimentum risus pharetra varius.</p>
-      </div>
-    </main>
-    <section class="w-full bg-blue-500 py-16">
-
-    </section>
+  <div class="relative overflow-hidden">
+    <img src="public/img/header.png" class="w-full">
+    <div class="absolute inset-0 flex items-end">
+      <div class="triangle1 translate-y-10 w-full h-[200px]"></div>
+    </div>
+    <div class="absolute inset-0 flex items-center text-center">
+      <p class="p-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet luctus tempor. Fusce consectetur tellus quis est hendrerit, accumsan posuere magna finibus. Ut ullamcorper feugiat felis ut aliquam. Suspendisse faucibus suscipit mauris scelerisque hendrerit. Nulla feugiat risus sit amet augue pellentesque accumsan. Sed sit amet semper nisi. Sed varius enim cursus augue placerat efficitur. Cras nec augue tempus, fermentum lacus et, molestie justo. Etiam finibus augue vel ligula porta, eget porta massa pretium. Phasellus scelerisque volutpat scelerisque. Donec non nibh augue. Phasellus facilisis orci id leo vestibulum sollicitudin. Suspendisse potenti.
+      </p>
+    </div>
   </div>
+
+
+  <main>
+<div class="bg-amber-700 pb-9 "></div>
+  </main>
+
 </template>
 <script setup lang="ts">
 </script>
