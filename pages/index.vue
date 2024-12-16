4c98d59d5f145451a2a3648964e72b1d52e15bae
Cocovg <Cocovanglabbeek@Hotmail.com>
2024-12-16 09:13:27 +0100

    2 shapes via img

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="relative overflow-hidden">
-    <img src="public/img/header.png" class="w-full">
+  <div class="relative">
+    <img src="public/img/header.png">
     <div class="absolute inset-0 flex items-end">
-      <div class="triangle1 translate-y-10 w-full h-[200px]"></div>
+      <img src="public/img/shape1.png" class="translate-y-80">
     </div>
-    <div class="absolute inset-0 flex items-center text-center">
+    <div class="absolute">
       <p class="p-4">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet luctus tempor. Fusce consectetur tellus quis est hendrerit, accumsan posuere magna finibus. Ut ullamcorper feugiat felis ut aliquam. Suspendisse faucibus suscipit mauris scelerisque hendrerit. Nulla feugiat risus sit amet augue pellentesque accumsan. Sed sit amet semper nisi. Sed varius enim cursus augue placerat efficitur. Cras nec augue tempus, fermentum lacus et, molestie justo. Etiam finibus augue vel ligula porta, eget porta massa pretium. Phasellus scelerisque volutpat scelerisque. Donec non nibh augue. Phasellus facilisis orci id leo vestibulum sollicitudin. Suspendisse potenti.
       </p>
     </div>
   </div>
 
+  <div class="block h-[1000px]">
+    <div class=" inset-0 flex items-end">
+      <img  src="public/img/shape2.png" >
+    </div>
+  </div>
 
   <main>
-<div class="bg-amber-700 pb-9 "></div>
-  </main>
+      <section class="w-full  bg-blue-500 py-16">
+
+      </section>
+    </main>
 
 </template>
 <script setup lang="ts">
